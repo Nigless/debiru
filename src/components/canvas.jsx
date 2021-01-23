1b7ebf9193f981b3bdf75ca6fd5b17c66273d9ca
Nigless <nigless.gaming@gmail.com>
2021-01-23 20:30:55 +0300

    Добавил компонент Canvas

---
+++ src/components/canvas.jsx
@@ -0,0 +1,19 @@
+import React, { createRef, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Canvas = styled.canvas({
+	width: '100%',
+});
+
+export default function (props) {
+	const canvas = createRef();
+
+	const { onload, className } = props;
+	useEffect(() => {
+		const elem = canvas.current;
+		elem.width = elem.offsetWidth;
+		elem.height = elem.offsetHeight;
+		onload(elem);
+	}, []);
+	return <Canvas ref={canvas} className={className} />;
+}
